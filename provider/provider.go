342b85f0b2cd0eae6f1cb7a11e43a1db944fed29
georgiygusev <georgiy.gus@yandex.ru>
2025-01-07 01:36:56 +0300

    added path validation

--- provider/provider.go
+++ provider/provider.go
@@ -16,9 +16,9 @@ type AuthProvider interface {
 	IsUserHaveRoles(roles []string, userRoles []string) bool
 	SerializeJwkSet(key jwk.Set) (string, error)
 	DeserializeJwkSet(serializedKey string) (jwk.Set, error)
-	Authorize(ctx context.Context, path string, tokenString string) (models.UserDetails, error)
-	AddEndpointSecurity(endpoint string, roles ...string)
-	IsEndpointSecure(endpoint string) bool
+	Authorize(ctx context.Context, path string, method string, tokenString string) (models.UserDetails, error)
+	AddEndpointSecurity(pathPattern string, methods []string, roles []string)
+	IsEndpointSecure(endpoint string, method string) bool
 }
 
 const (
